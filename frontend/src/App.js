ea08c62f71ed8f5c69b226d26891d1776f9e54d8
deutschkihun <deutschkihun@gmail.com>
2020-11-13 11:24:59 +0100

    frontend error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
-import React ,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import axios from 'axios'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('/api/hi')
+      .then(response => {
+        console.log('response', response)
+      })
+  }, [])
+
+  useEffect(() => {
+    //여기서 데이터베이스에 있는 값을 가져온다.
+    axios.get('/api/values')
+      .then(response => {
+        console.log('response', response)
+        setLists(response.data)
+      })
+  }, [])
+
 
   const [lists, setLists] = useState([])
   const [value, setValue] = useState("")
 
-  useEffect(() => {
-    axios.get('/api/values')
-      .then(response => {
-        console.log('response',response)
-        setLists(response.data)
-      })
-  }, [])
 
   const changeHandler = (event) => {
     setValue(event.currentTarget.value)
@@ -23,30 +33,38 @@ function App() {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    axios.post('/api/value', {value:value})
+    axios.post('/api/value', { value: value })
       .then(response => {
-        if(response.data.success) {
-            setLists([...lists,response.data])
-            setValue("")
-        }else{
-          alert('fail')
+        if (response.data.success) {
+          console.log('response', response)
+          setLists([...lists, response.data])
+          setValue("");
+        } else {
+          alert('값을 DB에 넣는데 실패했습니다.')
         }
       })
-
   }
-  
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <form onSubmit={submitHandler}> 
-            <input type="text"
-            placeholder="입력해주세요"
-            onChange={changeHandler}
-            value={value}/>
-            <button type="submit">확인</button>
+        <div className="container">
+
+          {lists && lists.map((list, index) => (
+            <li key={index}>{list.value} </li>
+          ))}
+          <br />
+            안녕하세요.
+          <form className="example" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="입력해주세요..."
+              onChange={changeHandler}
+              value={value}
+            />
+            <button type="submit">확인.</button>
           </form>
         </div>
       </header>
@@ -54,4 +72,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
